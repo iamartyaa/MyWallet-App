0f7fca776b4b67b09e4fb92a22f6b25c9448d7b1
Amartya Yadav <iamartyayadav@gmail.com>
2022-02-16 21:32:44 +0530

    automatic close bottomSheet

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors, prefer_const_constructors_in_immutables
 
 import 'package:flutter/material.dart';
 
@@ -26,6 +26,8 @@ class _NewTransactionState extends State<NewTransaction> {
     }
     widget.addNewTransaction(
         enteredTitle, enteredAmount);
+
+    Navigator.of(context).pop();
   }
 
   @override
